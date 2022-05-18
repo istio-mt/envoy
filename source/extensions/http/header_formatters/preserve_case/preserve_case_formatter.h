127205bd93b797e50233b4f31ffa92e33ab87eb6
jiangshantao <634142246@qq.com>
2022-05-18 14:21:04 +0800

    Feature/preserve case support formatter on unknown headers  (#20780) (#13)
    Signed-off-by: jiangshantao <jiangshantao-dbg@qq.com>
    
    Signed-off-by: jiangshantao <634142246@qq.com>

--- source/extensions/http/header_formatters/preserve_case/preserve_case_formatter.h
+++ source/extensions/http/header_formatters/preserve_case/preserve_case_formatter.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "envoy/common/exception.h"
+#include "envoy/extensions/http/header_formatters/preserve_case/v3/preserve_case.pb.h"
 #include "envoy/http/header_formatter.h"
 
 #include "common/common/utility.h"
@@ -14,12 +16,24 @@ namespace PreserveCase {
 class PreserveCaseHeaderFormatter : public Envoy::Http::StatefulHeaderKeyFormatter {
 public:
   // Envoy::Http::StatefulHeaderKeyFormatter
+  PreserveCaseHeaderFormatter(
+      const bool forward_reason_phrase,
+      const envoy::extensions::http::header_formatters::preserve_case::v3::
+          PreserveCaseFormatterConfig::FormatterTypeOnEnvoyHeaders formatter_type_on_envoy_headers);
+
   std::string format(absl::string_view key) const override;
   void processKey(absl::string_view key) override;
+  void setReasonPhrase(absl::string_view reason_phrase) override;
+  absl::string_view getReasonPhrase() const override;
+  Envoy::Http::HeaderKeyFormatterOptConstRef formatterOnEnvoyHeaders() const;
 
 private:
   StringUtil::CaseUnorderedSet original_header_keys_;
-  Envoy::Http::Http1::ProperCaseHeaderKeyFormatter proper_case_header_key_formatter_;
+  bool forward_reason_phrase_{false};
+  std::string reason_phrase_;
+  const envoy::extensions::http::header_formatters::preserve_case::v3::PreserveCaseFormatterConfig::
+      FormatterTypeOnEnvoyHeaders formatter_type_on_envoy_headers_;
+  Envoy::Http::HeaderKeyFormatterConstPtr header_key_formatter_on_enovy_headers_;
 };
 
 } // namespace PreserveCase
